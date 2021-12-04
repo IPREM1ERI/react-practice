a5baf0f72cbad10c1197b6feb081cab0f0117896
whlong1 <whunterlong@gmail.com>
2021-12-04 14:57:30 -0500

    solution code

---
+++ src/Components/PostOffice/NewLetter/SelectBox.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const SelectBox = (props) => {
+  return (
+    <select required name="boxNo" onChange={(e) => props.setBoxNum(e.target.value)}>
+      <option value="">Select PO Box</option>
+      {props.boxNumbers.map((num) => (
+        <option key={num} value={num}>PO Box {num}</option>
+      ))}
+    </select>
+  )
+}
+
+export default SelectBox
